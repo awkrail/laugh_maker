cc81654a13e110c28bc04e52a0271fb0a7cded3e
misogil0116 <bluecave339@icloud.com>
2017-03-28 17:48:33 +0900

    add trainer and 3 layer network

--- lstm.py
+++ lstm.py
@@ -26,6 +26,28 @@ def shuffle_ary(np_ary, label_ary):
 
     return af_np_ary, af_label_ary
 
+class LaughNet(chainer.Chain):
+    def __init__(self):
+        super(LaughNet, self).__init__(
+            l1=L.Linear(None, 200),
+            l2=L.Linear(None, 100),
+            l3=L.Linear(100, 2)
+        )
+
+    def __call__(self, x):
+        h = F.relu(self.l1(x))
+        h = F.relu(self.l2(h))
+        ans = self.l3(h)
+
+        return ans
+
+
+# model define
+model = LaughNet()
+classify_model = L.Classifier(model)
+optimizer = chainer.optimizers.Adam()
+optimizer.setup(classify_model)
+
 # row_data_csv_load
 np_csv0, np_label0 = load_raw_data(0)
 np_csv1, np_label1 = load_raw_data(1)
@@ -37,11 +59,41 @@ all_label = np_label0 + np_label1
 # after_shuffle_data
 all_ary, all_label = shuffle_ary(all_ary, all_label)
 
-# make_chainer_datasets
+# make_chainer_datasets and iterator
 threshold = np.int32(len(all_ary)/5*4)
 train = tuple_dataset.TupleDataset(all_ary[0:threshold], all_label[0:threshold])
 test = tuple_dataset.TupleDataset(all_ary[threshold:], all_label[threshold:])
 
+train_iter = chainer.iterators.SerialIterator(train, 100, shuffle=True)
+test_iter = chainer.iterators.SerialIterator(test, 100, repeat=False, shuffle=False)
+
+updater = training.StandardUpdater(train_iter, optimizer, device=-1)
+trainer = training.Trainer(updater, (100, 'epoch'), out='result')
+trainer.extend(extensions.Evaluator(test_iter, classify_model, device=-1))
+trainer.extend(extensions.LogReport())
+trainer.extend(extensions.PlotReport(y_keys='main/loss', file_name='main_loss.png'))
+trainer.extend(extensions.PlotReport(y_keys='main/accuracy', file_name='main_acc.png'))
+trainer.extend(extensions.PlotReport(y_keys='validation/main/loss', file_name='overfitting.png'))
+trainer.extend(extensions.PlotReport(y_keys='validation/main/accuracy', file_name='validation_acc.png'))
+
+trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss', 'main/accuracy', 'validation/main/accuracy']))
+
+trainer.run()
 
 
-print(train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
