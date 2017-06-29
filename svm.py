781ee09743fc6e64bc1cce1d21b27792b62568bd
misogil0116 <lack_un@yahoo.co.jp>
2017-06-29 19:56:40 +0900

    SVMで日常データをチェック, 結果はREADMEへ. まぁダメでした

--- svm.py
+++ svm.py
@@ -1,7 +1,7 @@
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 import numpy as np
-import pandas as pd
+from sklearn.externals import joblib
 import csv
 
 
@@ -23,83 +23,16 @@ class Ary4SvmClass(object):
             self.answer1 = [1 for _ in range(len(self.ary1))]
 
 
-# 適合率を測定します。1のデータだけ取り出す。
-class HighDimension4SvmClass(object):
-    """
-        適合率を測定する。
-        self.ary1 -> わらっているときのデータ
-        self.answer1 -> 1というデータセット
-    """
-    def __init__(self):
-        self.ary0 = []
-        self.ary1 = []
-        self.answer0 = []
-        self.answer1 = []
-
-    def load_csv(self):
-        with open('84Raw/0.csv', 'r') as f:
-            self.ary0 = [row for row in csv.reader(f)]
-
-        with open('84Raw/1.csv', 'r') as f:
-            self.ary1 = [row for row in csv.reader(f)]
-
-        self.ary0 = self.pd2ary(self.ary0)
-        self.answer0 = [0 for _ in range(len(self.ary0))]
-        self.ary1 = self.pd2ary(self.ary1)
-        self.answer1 = [1 for _ in range(len(self.ary1))]
-
-    @staticmethod
-    def pd2ary(self_ary):
-        count = 0
-        tmp_ary = []
-        return_ary = []
-        for row in self_ary:
-            if count == 50:
-                return_ary.append(np.array(tmp_ary, dtype=np.float32))
-                tmp_ary = []
-                count = 0
-                continue
-            mean = float(np.array(row, dtype=np.float32).mean()) # 平均を計算
-            tmp_ary.append(mean)
-            count += 1
-
-        return return_ary
-
-    def calculate_recall_rate(self, clf):
-        recall = 0
-        question_recall = len(self.answer1)
-
-        for data, test_answer in zip(self.ary1, self.answer1):
-            answer = clf.predict([data])
-            if answer == test_answer:
-                recall += 1
-
-        print('recall rate: ', recall / question_recall)
-
-    def calculate_accuracy_rate(self, clf):
-        all_ary = self.ary0 + self.ary1
-        all_answer = self.answer0 + self.answer1
-        accuracy = 0
-        question_acc = len(all_ary)
-
-        # import ipdb; ipdb.set_trace()
-
-        for data, test_answer in zip(all_ary, all_answer):
-            answer = clf.predict([data])
-            if answer == test_answer:
-                accuracy += 1
-        print('accuracy rate: ', accuracy / question_acc)
-
 def main():
     raw_data = Ary4SvmClass()
     raw_data.load_csv()
 
     # 全部のデータを獲得する
     all_data = np.array(raw_data.ary0 + raw_data.ary1, dtype=np.float32)
-    label_data = np.array(raw_data.answer0 + raw_data.answer1, dtype=np.float32)
+    label_data = np.array(raw_data.answer0 + raw_data.answer1, dtype=np.int32)
 
     # 学習用, テスト用にデータを分ける
-    x_train, x_test, y_train, y_test = train_test_split(all_data, label_data, test_size=0.33, random_state=30)
+    x_train, x_test, y_train, y_test = train_test_split(all_data, label_data, test_size=0.4, random_state=30)
 
     # import ipdb; ipdb.set_trace()
     clf = svm.SVC()
@@ -113,7 +46,6 @@ def main():
     recall = 0
     question_recall = len(raw_data.answer1)
 
-    """
     for test_data, test_answer in zip(x_test, y_test):
         answer = clf.predict([test_data])
         if test_answer == answer:
@@ -124,12 +56,9 @@ def main():
 
     print('accuracy rate:', accuracy / question_num)
     print('recall rate: ', recall / question_recall)
-    """
-    # 日常データで測定(適合率のみ)
-    daily_data = HighDimension4SvmClass()
-    daily_data.load_csv()
-    daily_data.calculate_accuracy_rate(clf)
-    daily_data.calculate_recall_rate(clf)
+
+    # うまくいかないので, validation_src/以下で検証。モデルをシリアライズする。
+    joblib.dump(clf, 'clf.pkl')
 
 
 if __name__ == '__main__':
---
+++ validation_src/test_svm.py
@@ -0,0 +1,67 @@
+from sklearn import svm
+import numpy as np
+from sklearn.externals import joblib
+import csv
+
+
+class Daily50dimClass(object):
+    def __init__(self):
+        self.daily_ary = []
+        self.label_ary = []
+        self.recall_num = 0
+        self.num_0 = 0
+
+    def load_csv(self):
+        with open('dim50_daily_fixed.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.daily_ary.append(row[0:50])
+                self.label_ary.append(int(row[50]))
+                if int(row[50]) == 1:
+                    self.recall_num += 1
+                else:
+                    self.num_0 += 1
+
+        self.daily_ary = np.array(self.daily_ary, dtype=np.float32)
+
+
+def main():
+    # loading dataset
+    daily_data = Daily50dimClass()
+    daily_data.load_csv()
+
+    # loading model
+    clf = joblib.load('clf.pkl')
+
+    accuracy = 0
+    question_num = len(daily_data.daily_ary)
+
+    recall = 0
+    answer_one_num = daily_data.recall_num
+    answer_zero_num = daily_data.num_0
+
+    answer_one = 0
+
+    for data, label in zip(daily_data.daily_ary, daily_data.label_ary):
+        answer = clf.predict([data])
+        if answer == label:
+            accuracy += 1
+            if answer == 1:
+                recall += 1
+
+    # import ipdb; ipdb.set_trace()
+
+    print('question_num: ', question_num)
+    print('answer_one_num: ', answer_one_num)
+    print('answer_zero_num: ', answer_zero_num)
+    print('daily data accuracy: ', accuracy / question_num)
+    print('recall rate: ', recall / answer_one_num)
+    print('answer one number: ', answer_one)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
